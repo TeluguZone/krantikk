e7ffd676e192b60a85d03167dbe147f1923deb41
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-12-26 23:19:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
 MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
-START_MESSAGE = environ.get('START_MESSAGE','Hello {mention}⚡️... I'm Stuff provider bot ... For Desi, Search As "Desi"... For Foreign, Search As "Foreign" ❤ Without ads stuff ki access kavali ante membership tisuko mava per month just 20rs a. For access contact me @STUFF_ACCESS_BOT ,slots will open every month 25th to 1th only.')
+START_MESSAGE = environ.get('START_MESSAGE','Hello {mention}⚡️... I'm Stuff provider bot ... For Desi, Search As Desi... For Foreign, Search As Foreign Without ads stuff ki access kavali ante membership tisuko mava per month just 20rs a. For access contact me @STUFF_ACCESS_BOT ,slots will open every month 25th to 1th only.')
 BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", "⚠️ 𝙃𝙚𝙮 {query}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣")
 FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', '𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑴𝒐𝒗𝒊𝒆 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒐 𝑼𝒔𝒆 𝑻𝒉𝒊𝒔 𝑩𝒐𝒕!')
 RemoveBG_API = environ.get("RemoveBG_API", "")
